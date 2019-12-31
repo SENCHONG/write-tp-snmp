b0262a0e045a07c616f8e1b4d19f15b9868fa7ae
chensch <chensch@aliyun.com>
2019-12-31 08:41:22 +0800

    update

--- snmp.py
+++ snmp.py
@@ -1,14 +1,61 @@
 from snmp_cmds import snmpwalk
+import json
+
+# 获取设备的名称
 
 
-def getInfo(ip_address, config):
-    res = snmpwalk(community='public',
-                      ipaddress=ip_address,
-                      oid="1.3.6.1.4.1.3320.127.1.4")
+def getSysName(ip_address, comu_str):
+    res = snmpwalk(community=comu_str,
+                   ipaddress=ip_address,
+                   oid="SNMPv2-MIB:sysName")
+    return res[0][1]
 
-    for line in res:
-        print(line[0], '     ', line[1])
-    return
+
+# 获取LLDP
+def getLLDP(ip_address, comu_str):
+    res = []
+    loc_desc = {}
+    i = 0
+    loc_name = getSysName(ip_address, comu_str)
+
+    # 邻居接口名称
+    lldpRemPortDesc_res = snmpwalk(community=comu_str,
+                                   ipaddress=ip_address,
+                                   oid="LLDP-MIB:lldpRemPortDesc")
+    # 邻居系统名称
+    lldpRemSysName_res = snmpwalk(community=comu_str,
+                                  ipaddress=ip_address,
+                                  oid="LLDP-MIB:lldpRemSysName")
+    # 本机接口名称
+    ifDescr_res = snmpwalk(community=comu_str,
+                           ipaddress=ip_address,
+                           oid="IF-MIB:ifDescr")
+    for line in ifDescr_res:
+        loc_indexs = line[0].split('.')
+        loc_desc.update(
+            {
+                loc_indexs[-1]: line[1]
+            }
+        )
+    while i < len(lldpRemPortDesc_res):
+        lldpRemPortDesc_indexs = lldpRemPortDesc_res[i][0].split('.')
+        res.append(
+            {
+                "local_interface_index": lldpRemPortDesc_indexs[-2],
+                "local_sysName": loc_name,
+                "local_interface": loc_desc[str(lldpRemPortDesc_indexs[-2])],
+                "remote_interface": lldpRemPortDesc_res[i][1].replace(" Interface", ""),
+                "remote_sysName": lldpRemSysName_res[i][1],
+                "remote_interface_index": lldpRemPortDesc_indexs[-1]
+            }
+
+        )
+        i += 1
+    # str_json = json.dumps(res, indent=2, ensure_ascii=False)
+    # print(res)
+    return res
+
+
 
 # interfaces_table_named_oid = [
 #     ('IF-MIB', 'ifDescr'),
@@ -26,6 +73,6 @@ def getInfo(ip_address, config):
 # lldp_table_named_oid = [
 #     ('LLDP-MIB', 'lldpRemSysName'),
 #     ('LLDP-MIB', 'lldpRemSysDesc'),
-#     ('LLDP-MIB', 'lldpRemPortId'),
+#     ('LLDP-MIB', 'lldpRemPortId'),lldpLocPortId
 #     ('LLDP-MIB', 'lldpRemPortDesc')
 # ]'SNMPv2-MIB', 'sysName'
--- write_tp_snmp.py
+++ write_tp_snmp.py
@@ -6,6 +6,8 @@ import snmp
 
 
 def snmpData():
+
+    res = {}
     config = configparser.ConfigParser()
     config.read('config.ini')
     config.sections()
@@ -13,9 +15,22 @@ def snmpData():
         print("错误：缺乏配置文件")
         exit(1)
     ip_address_list = json.loads(config["DEVICES"]["address"])
-
+    comu_str = config["DEFAULT"]["SnmpCommunityString"]
     for ip_address in ip_address_list:
-        ip_address = ip_address.replace("\n", '')
-        snmp.getInfo(ip_address, config)
+        try:
+            ip_address = ip_address.replace("\n", '')
+            _res = snmp.getLLDP(ip_address, comu_str)
+        except:
+            print("出现错误")
+            exit(-1)
+        else:
+            res.update({
+                snmp.getSysName(ip_address, comu_str): _res
+            })
+    str_json = json.dumps(res, indent=2, ensure_ascii=False)
+    print(str_json)
+
     return
+
+
 snmpData()
